0e0170555fa7e78af3bfa833b4a3fdff5765a9ff
Sergio Ruy <sergio_ruy@hotmail.com>
2020-09-02 04:02:16 -0300

    Desafio de modulo Pessoa concluído.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { DropdownModule } from 'primeng/dropdown';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoaPesquisaComponent } from './pessoas/pessoa-pesquisa/pessoa-pesquisa.component';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextModule } from 'primeng/inputtext';
-import { CalendarModule } from 'primeng/calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { MessageComponent } from './message/message.component';
-import { PessoaGridComponent } from './pessoas/pessoa-grid/pessoa-grid.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PessoaPesquisaComponent,
-    PessoaCadastroComponent,
-    MessageComponent,
-    PessoaGridComponent
+    MessageComponent
   ],
     imports: [
-        LancamentosModule,
-        InputMaskModule,
-        InputNumberModule,
-        DropdownModule,
-        SelectButtonModule,
-        BrowserAnimationsModule,
-        CalendarModule,
-        InputTextareaModule,
-        BrowserModule,
-        InputTextModule,
-        ButtonModule,
-        AppRoutingModule,
-        TableModule,
-        TooltipModule,
-        FormsModule
+      LancamentosModule,
+      PessoasModule,
+
+      BrowserAnimationsModule,
+      BrowserModule,
+
+      AppRoutingModule
+
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import {PessoaGridComponent} from "./pessoa-grid/pessoa-grid.component";
-import {PessoaPesquisaComponent} from "./pessoa-pesquisa/pessoa-pesquisa.component";
+import { PessoaGridComponent } from './pessoa-grid/pessoa-grid.component';
+import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
+
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 
 
@@ -14,7 +21,18 @@ import {PessoaPesquisaComponent} from "./pessoa-pesquisa/pessoa-pesquisa.compone
     PessoaPesquisaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+
+    InputMaskModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
+  ],
+  exports: [
+    PessoaPesquisaComponent,
+    PessoaCadastroComponent
   ]
 })
 export class PessoasModule { }
